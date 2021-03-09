1db25a247f5902234293fbe5137e8255d06e1873
David Redmin <david.redmin@trio.dhs.gov>
2021-03-08 23:10:00 -0500

    Create some useful local variables

---
+++ locals.tf
@@ -0,0 +1,26 @@
+# ------------------------------------------------------------------------------
+# Evaluate expressions for use throughout this configuration.
+# ------------------------------------------------------------------------------
+
+locals {
+  # If var.assume_role_policy_description contains two instances of "%s", use
+  # format() to replace the first "%s" with var.role_name and the second "%s"
+  # with var.terraform_account_name.  Otherwise just use
+  # var.assume_role_policy_description as is.
+  assume_role_policy_description = length(regexall(".*%s.*%s.*", var.assume_role_policy_description)) > 0 ? format(var.assume_role_policy_description, var.role_name, var.terraform_account_name) : var.assume_role_policy_description
+
+  # If var.assume_role_policy_name contains one instance of "%s", use format()
+  # to replace the "%s" with var.role_name. Otherwise just use
+  # var.assume_role_policy_name as is.
+  assume_role_policy_name = length(regexall(".*%s.*", var.assume_role_policy_name)) > 0 ? format(var.assume_role_policy_name, var.role_name) : var.assume_role_policy_name
+
+  # Properly format usernames for use in an ARN
+  iam_usernames = contains(var.iam_usernames, "root") ? ["root"] : formatlist("user/%s", var.iam_usernames)
+
+  # If var.role_description contains three instances of "%s", use format() to
+  # replace the first "%s" with var.terraform_workspace, the second "%s" with
+  # var.terraform_state_path, and the third "%s" with
+  # var.terraform_state_bucket_name.  Otherwise just use var.role_description
+  # as is.
+  role_description = length(regexall(".*%s.*%s.*%s.*", var.role_description)) > 0 ? format(var.role_description, var.terraform_workspace, var.terraform_state_path, var.terraform_state_bucket_name) : var.role_description
+}
