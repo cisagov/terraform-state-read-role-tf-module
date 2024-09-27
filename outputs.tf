98b5d053ca287ac2e31a636cd78c2e0a07a395eb
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-09-27 11:18:06 -0400

    Add read_only to module outputs
    Also update variable and output descriptions to use the word "access"
    instead of "read" since writing may be allowed based on the value of
    read_only.

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,19 @@
 output "assume_policy" {
-  description = "An array that is either empty (if no assume role policy was created) or contains a single element that is the policy allowing assumption of the role that can read the specified Terraform state."
+  description = "An array that is either empty (if no assume role policy was created) or contains a single element that is the policy allowing assumption of the role that can access the specified Terraform state."
   value       = aws_iam_policy.assume_read_terraform_state_role
 }
 
 output "policy" {
-  description = "The policy that can read the specified Terraform state."
+  description = "The policy that can access the specified Terraform state."
   value       = module.read_terraform_state.policy
 }
 
+output "read_only" {
+  description = "A Boolean value indicating whether or not the role and policy are read-only.  If false then the role and policy will allow write permissions."
+  value       = var.read_only
+}
+
 output "role" {
-  description = "The role that can read the specified Terraform state."
+  description = "The role that can access the specified Terraform state."
   value       = module.read_terraform_state.role
 }
